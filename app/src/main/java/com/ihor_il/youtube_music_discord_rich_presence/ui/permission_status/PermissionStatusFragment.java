2150add7a625b8a7bad3675c3855cedbdfe83f97
Ihor Lupyna <62049009+ihor-il@users.noreply.github.com>
2024-10-15 14:55:01 +0300

    implemented settings change

--- app/src/main/java/com/ihor_il/youtube_music_discord_rich_presence/ui/permission_status/PermissionStatusFragment.java
+++ app/src/main/java/com/ihor_il/youtube_music_discord_rich_presence/ui/permission_status/PermissionStatusFragment.java
@@ -1,15 +1,17 @@
 package com.ihor_il.youtube_music_discord_rich_presence.ui.permission_status;
 
-import static android.view.View.GONE;
-import static android.view.View.VISIBLE;
-
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -20,41 +22,57 @@ public class PermissionStatusFragment extends Fragment {
 
     private FragmentPermissionStatusBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        PermissionStatusViewModel vm = new ViewModelProvider(this)
-                .get(PermissionStatusViewModel.class);
+    private final ActivityResultLauncher<Intent> settingsActivityLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            o -> getViewModel().checkPermissionStatus()
+    );
 
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState
+    ) {
         binding = FragmentPermissionStatusBinding.inflate(inflater, container, false);
 
+        PermissionStatusViewModel vm = getViewModel();
         vm.getPermissionStatus().observe(getViewLifecycleOwner(), this::setPermissionStatusControls);
+        vm.checkPermissionStatus();
+
+        binding.openSettingsButton.setOnClickListener(v -> openSettings());
+
         return binding.getRoot();
     }
 
-    private void setPermissionStatusControls(boolean hasPermission) {
-        final Resources resources = getResources();
-
-        binding.status.setText(resources.getString(getTextStringId(hasPermission)));
-        binding.status.setTextColor(resources.getColor(getTextColorId(hasPermission)));
-        binding.openSettingsButton.setVisibility(getButtonVisibility(hasPermission));
+    private @NonNull PermissionStatusViewModel getViewModel() {
+        return new ViewModelProvider(this)
+                .get(PermissionStatusViewModel.class);
     }
 
-    private int getTextStringId(boolean hasPermission) {
-        return hasPermission
+    private void setPermissionStatusControls(boolean hasPermission) {
+        binding.status.setText(getTextString(hasPermission));
+        binding.status.setTextColor(getTextColor(hasPermission));
+    }
+
+    private @NonNull String getTextString(boolean hasPermission) {
+        int stringId = hasPermission
                 ? R.string.permission_status_allowed
                 : R.string.permission_status_restricted;
+
+        return getResources().getString(stringId);
     }
 
-    private int getTextColorId(boolean hasPermission)
-    {
-        return hasPermission
-                ? R.color.purple_500
-                : R.color.teal_700;
+    private int getTextColor(boolean hasPermission) {
+        int colorCode = hasPermission
+                ? com.google.android.material.R.attr.colorPrimary
+                : com.google.android.material.R.attr.colorError;
+
+        TypedValue value = new TypedValue();
+        Resources.Theme theme = this.requireActivity().getTheme();
+        theme.resolveAttribute(colorCode, value, true);
+        return ContextCompat.getColor(this.requireContext(), value.resourceId);
     }
 
-    private int getButtonVisibility(boolean hasPermission)
-    {
-        return hasPermission ? GONE : VISIBLE;
+    private void openSettings() {
+        final String INTENT_KEY = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
+        settingsActivityLauncher.launch(new Intent(INTENT_KEY));
     }
 
     @Override
--- app/src/main/java/com/ihor_il/youtube_music_discord_rich_presence/ui/permission_status/PermissionStatusViewModel.java
+++ app/src/main/java/com/ihor_il/youtube_music_discord_rich_presence/ui/permission_status/PermissionStatusViewModel.java
@@ -1,19 +1,40 @@
 package com.ihor_il.youtube_music_discord_rich_presence.ui.permission_status;
 
+import android.app.Application;
+import android.content.Context;
+import android.provider.Settings;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-public class PermissionStatusViewModel extends ViewModel {
+public class PermissionStatusViewModel extends AndroidViewModel {
 
     private final MutableLiveData<Boolean> mPermissionStatus;
 
-    public PermissionStatusViewModel() {
+    public PermissionStatusViewModel(Application application) {
+        super(application);
         mPermissionStatus = new MutableLiveData<>();
         mPermissionStatus.setValue(false);
     }
 
     public LiveData<Boolean> getPermissionStatus() {
         return mPermissionStatus;
+    }
+
+    public void checkPermissionStatus() {
+        Application app = getApplication();
+        mPermissionStatus.setValue(hasNotificationAccess(app));
+    }
+
+    private boolean hasNotificationAccess(Application application) {
+        String NOTIFICATION_ACCESS_KEY = "enabled_notification_listeners";
+        Context appContext = application.getApplicationContext();
+
+        String appsWithNotifAccess = Settings.Secure.getString(
+                appContext.getContentResolver(), NOTIFICATION_ACCESS_KEY
+        );
+
+        return appsWithNotifAccess.contains(appContext.getPackageName());
     }
 }
