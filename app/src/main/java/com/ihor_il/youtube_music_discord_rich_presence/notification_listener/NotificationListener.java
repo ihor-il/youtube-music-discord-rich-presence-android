97895a60ef7f5690f1e2e9a70a6c4f4ff25c8ff2
Ihor Lupyna <62049009+ihor-il@users.noreply.github.com>
2024-10-15 14:54:26 +0300

    added stub for notification listener service

---
+++ app/src/main/java/com/ihor_il/youtube_music_discord_rich_presence/notification_listener/NotificationListener.java
@@ -0,0 +1,6 @@
+package com.ihor_il.youtube_music_discord_rich_presence.notification_listener;
+
+import android.service.notification.NotificationListenerService;
+
+public class NotificationListener extends NotificationListenerService {
+}
