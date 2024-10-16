2312ebef1d666c23c962f8f68cc6fdb274fa597b
Ihor Lupyna <62049009+ihor-il@users.noreply.github.com>
2024-10-16 23:10:43 +0300

    implemented settings page

--- app/src/main/java/com/ihor_il/youtube_music_discord_rich_presence/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/ihor_il/youtube_music_discord_rich_presence/ui/settings/SettingsFragment.java
@@ -1,8 +1,6 @@
 package com.ihor_il.youtube_music_discord_rich_presence.ui.settings;
 
 import android.os.Bundle;
-import android.text.InputFilter;
-import android.text.Spanned;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +8,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.ihor_il.youtube_music_discord_rich_presence.databinding.FragmentSettingsBinding;
 import com.ihor_il.youtube_music_discord_rich_presence.helpers.NumberInRangeTextMask;
+
+import java.util.Arrays;
 
 public class SettingsFragment extends Fragment {
 
@@ -21,9 +22,24 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState
     ) {
-
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        setMasks();
 
+        SettingsViewModel vm = getViewModel();
+        binding.save.setOnClickListener((v) -> saveChanges());
+
+        bindObservables(vm);
+        vm.loadPreferences();
+
+        return binding.getRoot();
+    }
+
+    private SettingsViewModel getViewModel() {
+        return new ViewModelProvider(this)
+                .get(SettingsViewModel.class);
+    }
+
+    private void setMasks() {
         TextWatcher ipMask = new NumberInRangeTextMask(1, 255);
         binding.ipAddress1.addTextChangedListener(ipMask);
         binding.ipAddress2.addTextChangedListener(ipMask);
@@ -32,8 +48,28 @@ public class SettingsFragment extends Fragment {
 
         TextWatcher portMask = new NumberInRangeTextMask(1, 65_535);
         binding.port.addTextChangedListener(portMask);
+    }
 
-        return binding.getRoot();
+    private void saveChanges() {
+       SettingsViewModel vm = getViewModel();
+       String[] values = new String[] {
+           binding.ipAddress1.getText().toString(),
+           binding.ipAddress2.getText().toString(),
+           binding.ipAddress3.getText().toString(),
+           binding.ipAddress4.getText().toString(),
+           binding.port.getText().toString(),
+       };
+
+       int[] intValues = Arrays.stream(values).mapToInt(Integer::parseInt).toArray();
+       vm.savePreferences(intValues);
+    }
+
+    private void bindObservables(SettingsViewModel vm) {
+        vm.getIp1().observe(getViewLifecycleOwner(), (v) -> binding.ipAddress1.setText(v.toString()));
+        vm.getIp2().observe(getViewLifecycleOwner(), (v) -> binding.ipAddress2.setText(v.toString()));
+        vm.getIp3().observe(getViewLifecycleOwner(), (v) -> binding.ipAddress3.setText(v.toString()));
+        vm.getIp4().observe(getViewLifecycleOwner(), (v) -> binding.ipAddress4.setText(v.toString()));
+        vm.getPort().observe(getViewLifecycleOwner(), (v) -> binding.port.setText(v.toString()));
     }
 
     @Override
--- app/src/main/java/com/ihor_il/youtube_music_discord_rich_presence/ui/settings/SettingsViewModel.java
+++ app/src/main/java/com/ihor_il/youtube_music_discord_rich_presence/ui/settings/SettingsViewModel.java
@@ -1,19 +1,67 @@
 package com.ihor_il.youtube_music_discord_rich_presence.ui.settings;
 
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-public class SettingsViewModel extends ViewModel {
+public class SettingsViewModel extends AndroidViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<Integer> mIp1 = new MutableLiveData<>(0);
+    private final MutableLiveData<Integer> mIp2 = new MutableLiveData<>(0);
+    private final MutableLiveData<Integer> mIp3 = new MutableLiveData<>(0);
+    private final MutableLiveData<Integer> mIp4 = new MutableLiveData<>(0);
+    private final MutableLiveData<Integer> mPort = new MutableLiveData<>(0);
 
-    public SettingsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+    public SettingsViewModel(Application app) {
+        super(app);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void loadPreferences() {
+        SharedPreferences prefs = getApplication().getSharedPreferences(SettingsKeys.PREFERENCES_KEY, Context.MODE_PRIVATE);
+
+        mIp1.setValue(prefs.getInt(SettingsKeys.IP_1, 0));
+        mIp2.setValue(prefs.getInt(SettingsKeys.IP_2, 0));
+        mIp3.setValue(prefs.getInt(SettingsKeys.IP_3, 0));
+        mIp4.setValue(prefs.getInt(SettingsKeys.IP_4, 0));
+        mPort.setValue(prefs.getInt(SettingsKeys.PORT, 0));
+    }
+
+    public void savePreferences(int[] values) {
+        if (values.length != 5) return;
+
+        SharedPreferences prefs = getApplication().getSharedPreferences(SettingsKeys.PREFERENCES_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putInt(SettingsKeys.IP_1, values[0]);
+        editor.putInt(SettingsKeys.IP_2, values[1]);
+        editor.putInt(SettingsKeys.IP_3, values[2]);
+        editor.putInt(SettingsKeys.IP_4, values[3]);
+        editor.putInt(SettingsKeys.PORT, values[4]);
+
+        editor.apply();
+    }
+
+    public LiveData<Integer> getIp1() {
+        return mIp1;
+    }
+
+    public LiveData<Integer> getIp2() {
+        return mIp2;
+    }
+
+    public LiveData<Integer> getIp3() {
+        return mIp3;
+    }
+
+    public LiveData<Integer> getIp4() {
+        return mIp4;
+    }
+
+    public LiveData<Integer> getPort() {
+        return mPort;
     }
 }
